08ceb6e6ad35425184414f525d95c90c8f5d9220
Rayko <mrayyanadeel4@gmail.com>
2025-03-08 12:28:01 +0500

    calc

---
+++ abc.sh
@@ -0,0 +1,9 @@
+#! /bin/env bash
+
+read -p "enter a num: " num
+
+if (($num % 2)); then
+	echo "$num is odd."
+else 
+	echo "$num is even."
+fi
---
+++ calculator.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+
+calc(){
+	if [[ $# -ne 3	]]; then
+		echo "Usage: calc <num1> <num2> <oper>"
+		return 1
+	fi
+
+	case $3 in
+		+) echo "Result: $(($1 + $2))" ;;
+		-) echo "Result: $(($1 - $2))" ;;
+		\*) echo "Result: $(($1 * $2))" ;;
+		/)
+			if [[ $2 -eq 0 ]]; then
+				echo "Div by Zero"
+			else
+				echo "Result: $(echo "scale=2; $1 / $2" | bc)"
+			fi
+			;;
+		*) echo "inavlid operation" ;;
+	esac
+}
